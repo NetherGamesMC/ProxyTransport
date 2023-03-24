db38220b593094678d4053b1e794af4013a54704
larryTheCoder <larryPeter132@gmail.com>
2023-03-24 22:45:28 +0800

    Provide latency information using TCP_INFO.

--- src/main/java/org/nethergames/proxytransport/impl/TransportClientConnection.java
+++ src/main/java/org/nethergames/proxytransport/impl/TransportClientConnection.java
@@ -6,6 +6,7 @@ import dev.waterdog.waterdogpe.network.connection.codec.compression.CompressionA
 import dev.waterdog.waterdogpe.network.serverinfo.ServerInfo;
 import dev.waterdog.waterdogpe.player.ProxiedPlayer;
 import io.netty.channel.Channel;
+import io.netty.channel.epoll.EpollSocketChannel;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket;
 
@@ -13,8 +14,12 @@ import javax.crypto.SecretKey;
 
 public class TransportClientConnection extends BedrockClientConnection {
 
+    private final Channel channel;
+
     public TransportClientConnection(ProxiedPlayer player, ServerInfo serverInfo, Channel channel) {
         super(player, serverInfo, channel);
+
+        this.channel = channel;
     }
 
     @Override
@@ -30,5 +35,18 @@ public class TransportClientConnection extends BedrockClientConnection {
     @Override
     public void enableEncryption(@NonNull SecretKey secretKey) {
         // Encryption is generally not good in server-to-server scenarios
+    }
+
+    @Override
+    public long getPing() {
+        // TCP_INFO is only supported in linux-family operating system as for now.
+        // So far this is the best option to calculate the player latency
+        if (channel instanceof EpollSocketChannel epollChannel) {
+            var tcpInfo = epollChannel.tcpInfo();
+
+            return tcpInfo.lastAckRecv() - tcpInfo.lastAckSent();
+        } else {
+            return 0L;
+        }
     }
 }
