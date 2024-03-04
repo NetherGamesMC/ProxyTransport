820eee210c10f653449eb39d48389a3adca18a01
Tobias G <tobiasdevde@gmail.com>
2024-03-04 22:27:55 +0100

    Switch back to miliseconds & use NetworkStackLatencyPacket again for ping

--- src/main/java/org/nethergames/proxytransport/ProxyTransport.java
+++ src/main/java/org/nethergames/proxytransport/ProxyTransport.java
@@ -2,6 +2,7 @@ package org.nethergames.proxytransport;
 
 import dev.waterdog.waterdogpe.network.protocol.ProtocolCodecs;
 import dev.waterdog.waterdogpe.plugin.Plugin;
+import io.netty.incubator.codec.quic.Quic;
 import org.nethergames.proxytransport.integration.QuicTransportServerInfo;
 import org.nethergames.proxytransport.integration.TcpTransportServerInfo;
 import org.nethergames.proxytransport.utils.CodecUpdater;
@@ -11,12 +12,16 @@ public class ProxyTransport extends Plugin {
     @Override
     public void onStartup() {
         ProtocolCodecs.addUpdater(new CodecUpdater());
+        getLogger().info("Registered architecture: {} | {}", System.getProperty("os.name"), System.getProperty("os.arch"));
 
+        getLogger().info("System Properties: {}", System.getProperties().toString());
         try {
             Class.forName("io.netty.incubator.codec.quic.QuicheNativeStaticallyReferencedJniMethods");
+            Quic.ensureAvailability();
             getLogger().info("QUIC is supported");
-        } catch (ClassNotFoundException e) {
+        } catch (Exception e) {
             getLogger().info("QUIC is not supported / shading failed");
+            getLogger().error(e);
         }
 
         getLogger().info("ProxyTransport was started.");
